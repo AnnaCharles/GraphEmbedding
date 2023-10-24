97592a63a71694bb8278523e6669f057497642c4
AnnaCharles <annacharles2@ymail.com>
2023-10-24 11:24:11 +0200

    script in folder data to generate the formatted datas and modification of plot_embeddings to make it interactive

--- examples/node2vec_wiki.py
+++ examples/node2vec_wiki.py
@@ -4,15 +4,20 @@ import numpy as np
 from ge.classify import read_node_label, Classifier
 from ge import Node2Vec
 from sklearn.linear_model import LogisticRegression
-
+import plotly.express as px
 import matplotlib.pyplot as plt
+import matplotlib.legend as lgd
 import networkx as nx
 from sklearn.manifold import TSNE
+import csv
+import pandas as pd
 
 
 def evaluate_embeddings(embeddings):
-    X, Y = read_node_label('../data/wiki/wiki_labels.txt')
+    X, Y = read_node_label('../data/media_labels.txt')
+    print(len(X))
     tr_frac = 0.8
+    print("test")
     print("Training classifier using {:.2f}% nodes...".format(
         tr_frac * 100))
     clf = Classifier(embeddings=embeddings, clf=LogisticRegression())
@@ -20,7 +25,22 @@ def evaluate_embeddings(embeddings):
 
 
 def plot_embeddings(embeddings,):
-    X, Y = read_node_label('../data/wiki/wiki_labels.txt')
+    X, Y = read_node_label('../data/media_labels.txt')
+
+    labels = []
+    nodes = []
+    categories = []
+    for id in embeddings.keys():
+        nodes.append(id)
+        labels.append(Y[X.index(id)][0])
+
+        if len(Y[X.index(id)]) != 1:
+            if not "/fr" in Y[X.index(id)][1:]:
+                categories.append(" ".join(Y[X.index(id)][1:]))
+            else:
+                categories.append("NoCategory")
+        else:
+            categories.append("NoCategory")
 
     emb_list = []
     for k in X:
@@ -30,24 +50,34 @@ def plot_embeddings(embeddings,):
     model = TSNE(n_components=2)
     node_pos = model.fit_transform(emb_list)
 
+    with open("../data/medias.csv") as file:
+        with open("nodes.csv", "w") as csvfile:
+            headers = ["x", "y", "label", "category"]
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writeheader()
+            for idx in range(len(node_pos)):
+                writer.writerow(
+                    {"x": node_pos[idx, 0], "y": node_pos[idx, 1], "label": labels[idx], "category": categories[idx]})
+
     color_idx = {}
     for i in range(len(X)):
         color_idx.setdefault(Y[i][0], [])
         color_idx[Y[i][0]].append(i)
 
-    for c, idx in color_idx.items():
-        plt.scatter(node_pos[idx, 0], node_pos[idx, 1], label=c)
-    plt.legend()
-    plt.show()
+    data = pd.read_csv("nodes.csv")
+    fig = px.scatter(data, x="x", y="y", hover_data="label", color="category")
+
+    fig.show()
 
 
 if __name__ == "__main__":
-    G=nx.read_edgelist('../data/wiki/Wiki_edgelist.txt',
-                         create_using = nx.DiGraph(), nodetype = None, data = [('weight', int)])
-    model = Node2Vec(G, walk_length=10, num_walks=80,
+    G = nx.read_edgelist('../data/media_edgelist.txt',
+                         create_using=nx.MultiDiGraph(), nodetype=None)
+    print(len(G.nodes))
+    model = Node2Vec(G, walk_length=10, num_walks=100,
                      p=0.25, q=4, workers=1, use_rejection_sampling=0)
-    model.train(window_size = 5, iter = 3)
-    embeddings=model.get_embeddings()
+    model.train(window_size=5, iter=3)
+    embeddings = model.get_embeddings()
 
-    evaluate_embeddings(embeddings)
+    # evaluate_embeddings(embeddings)
     plot_embeddings(embeddings)
--- examples/struc2vec_flight.py
+++ examples/struc2vec_flight.py
@@ -1,13 +1,13 @@
 import numpy as np
+import plotly.express as px
 
-
-
-from ge.classify import read_node_label,Classifier
+import csv
+import pandas as pd
+from ge.classify import read_node_label, Classifier
 
 from ge import Struc2Vec
 
 from sklearn.linear_model import LogisticRegression
-
 
 
 import matplotlib.pyplot as plt
@@ -17,10 +17,10 @@ import networkx as nx
 from sklearn.manifold import TSNE
 
 
-
 def evaluate_embeddings(embeddings):
 
-    X, Y = read_node_label('../data/flight/labels-brazil-airports.txt',skip_head=True)
+    X, Y = read_node_label(
+        '../data/flight/labels-brazil-airports.txt', skip_head=True)
 
     tr_frac = 0.8
 
@@ -33,56 +33,60 @@ def evaluate_embeddings(embeddings):
     clf.split_train_evaluate(X, Y, tr_frac)
 
 
-
-
-
 def plot_embeddings(embeddings,):
+    X, Y = read_node_label('../data/media_labels.txt')
 
-    X, Y = read_node_label('../data/flight/labels-brazil-airports.txt',skip_head=True)
+    # pour avoir les labels dans l'odre des noeuds dans l'embedding qui est différent de l'ordre initial
+    labels = []
+    nodes = []
+    categories = []
+    for id in embeddings.keys():
+        nodes.append(id)
+        labels.append(Y[X.index(id)][0])
 
-
+        if len(Y[X.index(id)]) != 1:
+            if not "/fr" in Y[X.index(id)][1:]:
+                categories.append(" ".join(Y[X.index(id)][1:]))
+            else:
+                categories.append("NoCategory")
+        else:
+            categories.append("NoCategory")
 
     emb_list = []
-
     for k in X:
-
         emb_list.append(embeddings[k])
-
     emb_list = np.array(emb_list)
 
-
-
     model = TSNE(n_components=2)
-
     node_pos = model.fit_transform(emb_list)
 
-
+    with open("../data/medias.csv") as file:
+        with open("nodes.csv", "w") as csvfile:
+            headers = ["x", "y", "label", "category"]
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writeheader()
+            for idx in range(len(node_pos)):
+                writer.writerow(
+                    {"x": node_pos[idx, 0], "y": node_pos[idx, 1], "label": labels[idx], "category": categories[idx]})
 
     color_idx = {}
-
     for i in range(len(X)):
-
         color_idx.setdefault(Y[i][0], [])
-
         color_idx[Y[i][0]].append(i)
 
+    data = pd.read_csv("nodes.csv")
+    fig = px.scatter(data, x="x", y="y", hover_data="label", color="category")
 
+    fig.show()
 
-    for c, idx in color_idx.items():
-
-        plt.scatter(node_pos[idx, 0], node_pos[idx, 1], label=c)  # c=node_colors)
-
-    plt.legend()
-
-    plt.show()
 
 if __name__ == "__main__":
-    G = nx.read_edgelist('../data/flight/brazil-airports.edgelist', create_using=nx.DiGraph(), nodetype=None,
-                         data=[('weight', int)])
+    G = nx.read_edgelist('../data/media_edgelist.txt',
+                         create_using=nx.MultiDiGraph(), nodetype=None)
 
     model = Struc2Vec(G, 10, 80, workers=4, verbose=40, )
     model.train()
     embeddings = model.get_embeddings()
 
-    evaluate_embeddings(embeddings)
+    # evaluate_embeddings(embeddings)
     plot_embeddings(embeddings)
