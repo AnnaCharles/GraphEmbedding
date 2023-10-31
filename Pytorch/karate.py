509583b018a160f8eed5ea20c71abdca2abcc58e
AnnaCharles <annacharles2@ymail.com>
2023-10-31 14:13:59 +0100

    Projection of DeepWalk and the clustering of Kmeans

--- Pytorch/karate.py
+++ Pytorch/karate.py
@@ -4,6 +4,8 @@ from sklearn.manifold import TSNE
 from collections import defaultdict, Counter
 import csv
 import plotly.express as px
+from sklearn.cluster import KMeans
+import numpy as np
 
 node_label=defaultdict()  #key : id du noeud, value : nom du noeud 
 node_class=defaultdict()  #key : id du noeud, value : nom de la categorie
@@ -17,34 +19,31 @@ with open("media_labels.csv") as file :
 graph=nx.read_gexf("medias_Fr_07_23.gexf")
 
 id=graph.nodes
+
 #remplace les id par des index 
 graph_mod=nx.DiGraph()
-#graph_mod.add_nodes_from([i for i in range(len(id))])
-
+graph_mod.add_nodes_from([i for i in range(len(id))])
 nodes = Counter()  #id de noeud, index dans l'array des noeuds
 count=0
 for idx in id:
     if node_class[int(idx)] !="":
         nodes[idx]=count
-        graph_mod.add_node(count)
+        #graph_mod.add_node(count)
         count+=1
-print(nodes)
-print(len(nodes))
+
 
 for u,v in graph.edges :
-    if node_class[int(u)] !="" and node_class[int(v)] !="":
-        #index_u=list(graph.nodes).index(u)
-        #index_v=list(graph.nodes).index(v)
-        index_u=nodes[u]
-        index_v=nodes[v]
-        graph_mod.add_edge(index_u, index_v)
+    index_u=list(graph.nodes).index(u)
+    index_v=list(graph.nodes).index(v)
+    #index_u=nodes[u]
+    #index_v=nodes[v]
+    graph_mod.add_edge(index_u, index_v)
 
 model = DeepWalk()
 model.fit(graph_mod)
 embedding = model.get_embedding()
-print(len(embedding))
 
-
+print(embedding)
 z = TSNE(n_components=3,perplexity=5).fit_transform(embedding)
 
 coord_x=[]
@@ -54,13 +53,40 @@ categories=[]
 labels=[]
 index=[]
 
-for i,id in enumerate(nodes.keys()) :
-    coord_x.append(z[i, 0])
-    coord_y.append(z[i, 1])
-    coord_z.append(z[i, 2])
-    categories.append(node_class[int(id)])
+for idx in graph_mod.nodes :
+    coord_x.append(z[idx, 0])
+    coord_y.append(z[idx, 1])
+    coord_z.append(z[idx, 2])
+    idd = list(id)[idx]
+    categories.append(node_class[int(idd)])
     index.append(id)
-    labels.append(node_label[int(id)])
+    labels.append(node_label[int(idd)])
 
-fig = px.scatter_3d(x=coord_x, y=coord_y,z=coord_z, hover_name=labels, color=categories)
+fig = px.scatter_3d(x=coord_x, y=coord_y,z=coord_z, hover_name=labels, color=categories, color_discrete_map = {"": "grey"})
+fig.show()
+
+#clustering
+X=np.array(embedding)
+kmeans = KMeans(n_clusters=15, random_state=0, n_init="auto").fit(X)
+
+
+centers = kmeans.cluster_centers_
+
+coord_x=[]
+coord_y=[]
+coord_z=[]
+categories=[]
+labels=[]
+index=[]
+for idx in graph_mod.nodes :
+    coord_x.append(z[idx, 0])
+    coord_y.append(z[idx, 1])
+    coord_z.append(z[idx, 2])
+    idd = list(id)[idx]
+    categories.append(node_class[int(idd)])
+    index.append(id)
+    labels.append(node_label[int(idd)])
+
+col=[str(i) for i in list(kmeans.labels_)]
+fig = px.scatter_3d(x=coord_x, y=coord_y,z=coord_z, hover_name=labels, color=col)
 fig.show()
